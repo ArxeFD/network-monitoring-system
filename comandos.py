c32ba9a64b61dffd4496442a4a16f297643040bc
DedoDePescado03 <gaelloretomiranda@gmail.com>
2024-04-30 08:25:19 -0700

    Agruegué más comandos

---
+++ comandos.py
@@ -0,0 +1,51 @@
+from netmiko import ConnectHandler
+
+device = {
+    'device-type':'cisco_ios',
+    'host':'ip del dispositivo',
+    'username':'gmedina',
+    'password':'cisco'
+}
+
+def banner1():#Opción 1 de un banner para el dispositivo
+    conn = ConnectHandler(**device)
+    conn.send_command("banner motd # ACCESO PROHIBIDO A PERSONAL NO AUTORIZADO #")
+    conn.disconnect()
+
+def banner2():#Opción 2 de un banner para el dispositivo
+    conn = ConnectHandler(**device)
+    conn.send_command("banner motd # SOLO ACCESO AUTORIZADO #")
+    conn.disconnect()
+
+def hostname():#Canbiar el hostname del dispositivo
+    hostname = 'Lo que ponga el usuario'
+    conn = ConnectHandler(**device)
+    conn.send_command("hostname " +  hostname)
+    conn.disconnect()
+
+def passEncryption():#Cifrar las contraseñas
+    conn = ConnectHandler(**device)
+    conn.send_command("service password encryption")
+    conn.disconnect()
+
+def showVersion():
+    conn = ConnectHandler(**device)
+    output = conn.send_command("show version")#Se guarda la información del dispositivo
+    conn.disconnect()
+
+def showConfig():
+    conn = ConnectHandler(**device)
+    output = conn.send_command("show ru")#Se guarda la configuración del dispositivo
+    conn.disconnect()
+
+def showInterfaceBrief():
+    conn = ConnectHandler(**device)
+    output = conn.send_command("show ip int br")#Se guarda la información de las interfaces del dispositivo
+    conn.disconnect()
+
+
+
+
+
+
+
