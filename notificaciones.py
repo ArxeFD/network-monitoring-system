e1d91500e3c47f3242ab4f56f702254548fa89e1
DedoDePescado03 <gaelloretomiranda@gmail.com>
2024-04-30 07:15:07 -0700

    Agregué función para mandar sms (no me funciona la librería de twilio, por eso está comentado)

--- notificaciones.py
+++ notificaciones.py
@@ -7,7 +7,7 @@ def EnviarCorreo():
     remitente = 'teconnect.uag@outlook.com'
     password = 'vwfmttkirbhxllzs'
     destinatario = 'gaelloretomiranda@gmail.com'
-    mensaje = 'Se detectó una desconexión en el router 2'
+    mensaje = 'Mensaje de prueba'
 
     email = EmailMessage()
     email["From"] = remitente
@@ -21,16 +21,24 @@ def EnviarCorreo():
     smtp.sendmail(remitente,destinatario,email.as_string())
     #Puede que el correo llegue en spam
     smtp.quit()
-def Whatsapp():
-    account_sid = "AC250cc50f0ae7c98fa5ae906ff9a8310b"
-    auth_token = "e64b29c3bf0be88f054ae8cbf4ed4170"
-    twilio_number = "+12183187909"
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body="Mensaje de prueba",
-        from_=twilio_number,
-        to="+523322068206"
-    )
+
+#def ´SMS():
+    #account_sid = 'AC250cc50f0ae7c98fa5ae906ff9a8310b'
+    #auth_token = 'AC250cc50f0ae7c98fa5ae906ff9a8310b'
+    #twilio_number = '+12183187909'
+    #target = '+523322068206'
+    
+    #client = Client(account_sid,auth_token)
+
+    #message = client.messages.create(
+        #body='Test',
+        #from_= twilio_number,
+        #to = target
+    #)
+
+    #print(message.body)
+
+
 
 EnviarCorreo()
-Whatsapp()
+#MSMS()
